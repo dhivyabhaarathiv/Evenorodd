32ba3f97a5efa3f15c84f16840d82814280fc725
dhivyabhaarathiv <dhivyabhaarathiv@gmail.com>
2017-06-14 14:24:55 +0530

    Create Evenorodd.java

---
+++ Evenorodd.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Evenorodd
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%2==0)
+        {
+            System.out.println("the given number is even");
+        }
+        else
+        {
+            System.out.println("the given number is odd");
+        }
+        
+    }
+}
+
